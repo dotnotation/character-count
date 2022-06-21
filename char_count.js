bab813ad5ea77e19d7899a6e132010daa3b89524
dotnotation <dotinmotionproductions@gmail.com>
2022-06-21 17:52:03 -0400

    added notes and started refactoring code

--- char_count.js
+++ char_count.js
@@ -12,3 +12,18 @@ function charCount(str){
     }
     return obj
 }
+
+// not great to use a for loop
+// better to use a for of loop as it will give you each character immediately
+// the if else statement is very basic and could be in a more simple syntax 
+
+function charCount(str) {
+    var obj = {}
+    for (var char of str) {
+        char = char.toLowerCase()
+        if (/[a-z0-9]/.test(char)){
+            obj[char] = ++obj[char] || 1
+        }
+    }
+    return obj
+}
